588235ea9b94b7745b5ccad9d63e6576d7a0c975
Lucas <me@lucasleite.us>
2022-07-21 18:08:38 -0400

    Add some styling to login page

--- components/LoginView.tsx
+++ components/LoginView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet, Alert} from 'react-native';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
 import {useDispatch} from 'react-redux';
 import {setAccessToken} from '../state/appSlice';
 import {auth0} from './Authentication';
@@ -12,7 +12,6 @@ const LoginView = () => {
         scope: 'openid profile email',
       })
       .then((credentials: any) => {
-        Alert.alert('AccessToken: ' + credentials.accessToken);
         dispatch(setAccessToken(credentials.accessToken));
       })
       .catch((error: any) => console.log(error));
@@ -20,14 +19,31 @@ const LoginView = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}> Auth0Sample - Login </Text>
-      <Button onPress={onLogin} title={'Log In'} />
+      <Pressable style={styles.button} onPress={onLogin}>
+        <Text style={styles.buttonText}>Log In</Text>
+      </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 45,
+    elevation: 2,
+    height: 90,
+    width: 200,
+    backgroundColor: '#2196F3',
+  },
+  buttonText: {color: 'white', fontSize: 25},
+  centeredView: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 22,
+  },
   container: {
+    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'black',
